d0e97428ebe0e8b70ae2e147c220fb46c3aaaf7e
Gcina Mbabe <gcinambabe20@gmail.com>
2025-03-28 00:03:19 +0200

    gc

---
+++ src/test/java/za/ac/cput/domain/CustomerTest.java
@@ -0,0 +1,38 @@
+package za.ac.cput.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void setAddress() {
+    }
+
+    @Test
+    void getPayment() {
+    }
+
+    @Test
+    void setPayment() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ src/test/java/za/ac/cput/domain/ProductTest.java
@@ -0,0 +1,46 @@
+package za.ac.cput.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getOrderIdentification() {
+    }
+
+    @Test
+    void setOrderIdentification() {
+    }
+
+    @Test
+    void getProductDetails() {
+    }
+
+    @Test
+    void setProductDetails() {
+    }
+
+    @Test
+    void getProductList() {
+    }
+
+    @Test
+    void setProductList() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
