d035c6e2b32bdefb4baece1ca458ba50cd644f38
Gcina Mbabe <gcinambabe20@gmail.com>
2025-03-28 13:57:56 +0200

    gc

---
+++ src/test/java/za/ac/cput/factory/ProductFactoryTest.java
@@ -0,0 +1,26 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductFactoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createProduct() {
+    }
+
+    @Test
+    void main() {
+    }
+}
